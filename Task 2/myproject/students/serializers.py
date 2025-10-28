1b192d489c5e2da6de005f4e37a7d1eaf7f87319
Mohammed R Umar Farooq <umarmd2k22@gmail.com>
2025-10-28 19:23:47 +0530

    Update serializers.py

--- Task 2/myproject/students/serializers.py
+++ Task 2/myproject/students/serializers.py
@@ -1,13 +1,7 @@
-# students/serializers.py
-
 from rest_framework import serializers
 from .models import Student
 
 class StudentSerializer(serializers.ModelSerializer):
-    # ModelSerializer is a shortcut to automatically generate fields
-    # corresponding to the model fields.
     class Meta:
         model = Student
-        # The 'id' is necessary to uniquely identify students (auto-created by Django)
         fields = ['id', 'name', 'age', 'email'] 
-        # Alternatively, use fields = '__all__' to include all model fields
