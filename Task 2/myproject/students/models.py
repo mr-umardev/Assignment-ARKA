243619edcee997a1db0e218d3064f1d3a14e22d4
Mohammed R Umar Farooq <umarmd2k22@gmail.com>
2025-10-28 19:23:24 +0530

    Update models.py

--- Task 2/myproject/students/models.py
+++ Task 2/myproject/students/models.py
@@ -1,18 +1,11 @@
-# students/models.py
-
 from django.db import models
 
 class Student(models.Model):
-    # CharField is used for short-to-medium length strings
     name = models.CharField(max_length=100) 
     
-    # IntegerField is used for whole numbers like age
     age = models.IntegerField()
     
-    # EmailField validates the input as an email address
-    # unique=True prevents duplicate email entries
     email = models.EmailField(unique=True) 
 
     def __str__(self):
-        # A human-readable representation, useful in the Django Admin
         return self.name
